bde879371373a9712365f8fb6f1fe8ecad1a8969
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-14 15:31:51 +0100

    fatto il terzo esercizio dei join

--- query-videogiochi.sql
+++ query-videogiochi.sql
@@ -123,9 +123,30 @@ ORDER BY avg_ratio DESC
 
 --1- Selezionare i dati di tutti giocatori che hanno scritto almeno una recensione, mostrandoli una sola volta (996)
 
+SELECT DISTINCT player_id, players.name, players.lastname, players.nickname, players.city
+FROM players
+INNER JOIN reviews 
+ON players.id = reviews.player_id
+
 --2- Sezionare tutti i videogame dei tornei tenuti nel 2016, mostrandoli una sola volta (226)
 
+SELECT DISTINCT videogames.id, videogames.name
+FROM videogames
+INNER JOIN tournament_videogame 
+ON videogames.id = tournament_videogame.videogame_id
+INNER JOIN tournaments 
+ON tournament_videogame.tournament_id = tournaments.id
+WHERE tournaments.year = 2016
+
 --3- Mostrare le categorie di ogni videogioco (1718)
+
+SELECT videogames.id, videogames.name, categories.name
+FROM videogames
+INNER JOIN category_videogame
+ON videogames.id = category_videogame.videogame_id
+INNER JOIN categories
+ON category_videogame.category_id = categories.id
+ORDER BY videogames.id
 
 --4- Selezionare i dati di tutte le software house che hanno rilasciato almeno un gioco dopo il 2020, mostrandoli una sola volta (6)
 
