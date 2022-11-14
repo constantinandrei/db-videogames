0fd7f294660c4c3c89d67bcb2128b0b228bf7b36
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-14 14:29:40 +0100

    completata prima parte

--- query-videogiochi.sql
+++ query-videogiochi.sql
@@ -66,8 +66,15 @@ GROUP BY videogame_id
 
 --11- Selezionare il numero e la media delle recensioni per il videogioco con ID = 412 (review number = 12, avg_rating = 3)
 
---12- Selezionare il numero di videogame che la software house con ID = 1 ha rilasciato nel 2018 (13
+SELECT COUNT(id) as reviews_number, AVG(rating) as avg_ratio
+FROM reviews 
+WHERE videogame_id = 412
+GROUP BY videogame_id
 
+--12- Selezionare il numero di videogame che la software house con ID = 1 ha rilasciato nel 2018 (13)
+SELECT COUNT(id)
+FROM videogames
+WHERE software_house_id = 1 AND DATEPART(year, release_date) = 2018 
 
 --QUERY CON GROUPBY
 
